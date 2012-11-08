593f877385d1c251393fe8e5246a5e1077bda254
slowry <slowry@MinistryOfInformation.(none)>
2012-11-08 16:40:58 +0100

    Filled Signal Headerfile

--- signals.cpp
+++ signals.cpp
@@ -1,1 +1,6 @@
+/** Signal Class CPP File
+created by Benibr  **/
+
+#include "signals.h"
+
 
--- signals.h
+++ signals.h
@@ -1,1 +1,39 @@
+/** Signal Class Header File
+created by Benibr**/
 
+#ifndef SIGNAL_HEADER
+#define SIGNAL_HEADER
+
+#include <string>
+#include <iostream>
+
+enum signalTypen {in, intern, out, unknown};
+
+using namespace std;
+
+class Signal
+{
+    public:
+        Signal();
+        ~Signal();
+        signalTypen getSignalTyp();
+        int getAnzahlZiele();
+        string getQuelle();
+        string getQuellenTyp();
+        string getZiel(int pos);
+        signalTypen setSignalTyp(signalTypen sigTyp);
+        void setQuelle(string gatterName);
+        void setQuellentyp(string gatterTyp);
+        void setAnzahlZiele(int nZiele);
+
+    protected:
+    private:
+        string quelle;
+        string quellenTyp;
+        string ziele;
+        int anzahlZiele;
+        signalTypen signalTyp;
+};
+
+
+#endif
