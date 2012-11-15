cab6508c10c288906fdca18a72457ffda3532bec
cee5 <co@co.(none)>
2012-11-15 14:51:22 +0100

    Flipflop

---
+++ Flipflop.cpp
@@ -0,0 +1,11 @@
+#include "Flipflop.h"
+
+Flipflop::Flipflop()
+{
+    //ctor
+}
+
+Flipflop::~Flipflop()
+{
+    //dtor
+}
---
+++ Flipflop.h
@@ -0,0 +1,14 @@
+#ifndef FLIPFLOP_H
+#define FLIPFLOP_H
+
+
+class Flipflop
+{
+    public:
+        Flipflop();
+        virtual ~Flipflop();
+    protected:
+    private:
+};
+
+#endif // FLIPFLOP_H
