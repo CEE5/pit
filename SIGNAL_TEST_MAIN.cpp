8ddfaccc45cd250dff27c2a1f7f46703cff0a69e
slowry <slowry@MinistryOfInformation.(none)>
2012-11-26 03:19:56 +0100

    added test main for signals & signallisteerzeuger

---
+++ SIGNAL_TEST_MAIN.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "signals.h"
+#include "SignalListeErzeuger.h"
+
+using namespace std;
+
+int main()
+{
+    string longcat;
+    cout << "File name:";
+    cin >> longcat;
+    SignalListeErzeuger* testlist = new SignalListeErzeuger(longcat);
+    cout << "Read: " << testlist->getDatei() << endl;
+    cout << "Vectorcontent:"  << endl;
+    for (int i=0;i<19;i++) {
+        cout << "---------------------------------------------\n";
+        cout << "Nummer: " << i << endl;
+        cout << "Signaltyp: " << testlist->getSignal(i)->getSignalTyp() << endl;
+        cout << "Quelle: " << testlist->getSignal(i)->getQuelle() << endl;
+        cout << "Quellentyp: " << testlist->getSignal(i)->getQuellenTyp() << endl;
+        cout << "Anzahlziele: " << testlist->getSignal(i)->getAnzahlZiele() << endl;
+        for (int i1=0;i1<testlist->getSignal(i)->getAnzahlZiele();i1++) {
+            cout << "-----" << testlist->getSignal(i)->getZiel(i1) <<endl;
+        }
+    }
+
+    return 0;
+}
