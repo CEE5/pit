1392543d0296658250360c8c79084a937478b754
cee5 <co@co.(none)>
2012-11-28 18:23:41 +0100

    bib kommentiert

--- Bibliothek.cpp
+++ Bibliothek.cpp
@@ -89,7 +89,7 @@ void Bibliothek::dateiAuswerten(void)
                 if(buffer=="\r")
                 {
 
-                    DEBUG_METHOD("leerzeile gefunden");
+                    DEBUG_METHOD("Leerzeile gefunden");
                     break;
                 }
 
@@ -126,114 +126,111 @@ void Bibliothek::dateiAuswerten(void)
         }
         else if(buffer.find("[")==0)
         {
-
+            //Kalmmern [ ] entfernen
             string name = buffer.substr(1,buffer.size()-2);
+
             //sucht in bibElemente nach dem im Absatz gefundenen Element
             for (vector<GatterTyp>::iterator it = bibElemente.begin(); it!=bibElemente.end(); ++it)
             {
+                //Element gefunden
                 if(it->getName()==name)
                 {
-                    DEBUG_METHOD(it->getName()<<"=="<<name);
+                    DEBUG_METHOD("Uebereinstimmung gefunden: "<<it->getName()<<"=="<<name);
+
 
                     while (!f.eof())
                     {
                         getline(f,buffer);
 
+                        //Abbruch falls Absatz zu Ende
                         if(buffer=="\r")
                         {
-                            DEBUG_METHOD("Ende von: "<<name<<" gefunden");
+                            DEBUG_METHOD("Ende von: "<<name<<" gefunden"<<endl);
                             break;
                         }
                         //"\r" entfernen
                         buffer.erase(buffer.size()-1);
 
 
+
+                        ///*allgemeine Attribute
+                        if(buffer.find("ei:")==0)
                         {
-                            //allgemeine Attribute
-                            if(buffer.find("ei:")==0)
-                            {
-                                it->setEingaenge(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD( "ei init "<<it->getEingaenge());
-                            }
+                            it->setEingaenge(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD( "ei init "<<it->getEingaenge());
+                        }
 
-                            else if(buffer.find("cl:")==0)
-                            {
-                                it->setLastKapazitaet(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD("cl init "<<it->getLastKapazitaet());
-                            }
+                        else if(buffer.find("cl:")==0)
+                        {
+                            it->setLastKapazitaet(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD("cl init "<<it->getLastKapazitaet());
+                        }
 
-                            else if(buffer.find("kl:")==0)
-                            {
-                                it->setLastFaktor(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD("kl init "<<it->getLastFaktor());
-                            }
+                        else if(buffer.find("kl:")==0)
+                        {
+                            it->setLastFaktor(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD("kl init "<<it->getLastFaktor());
+                        }
 
-                            else if(buffer.find("tpd0:")==0)
-                            {
-                                it->setGrundLaufzeit(atof(buffer.substr(5).c_str()));
-                                DEBUG_METHOD("tpd0 init "<<it->getGrundLaufzeit());
-                            }
-
-
-
-
-                            else if(buffer.find("tsetup:")==0)
-                            {
-                                ((Flipflop*)&it )->setSetupTime(atoi(buffer.substr(7).c_str()));
-                                DEBUG_METHOD("ff testup init: "<<((Flipflop*)&it )->getSetupTime());
-
-                            }
-                            else if(buffer.find("ed:")==0)
-                            {
-                                ((Flipflop*)&it )->setEingaenge(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD("ff ed init: "<<((Flipflop*)&it )->getEingaenge());
-
-                            }
-                            else if(buffer.find("thold:")==0)
-                            {
-                                ((Flipflop*)&it )->setHoldTime(atoi(buffer.substr(6).c_str()));
-                                DEBUG_METHOD("ff thold init: "<<((Flipflop*)&it )->getHoldTime());
-
-                            }
-                            else if(buffer.find("cd:")==0)
-                            {
-                                ((Flipflop*)&it )->setLastKapazitaet(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD("ff cd init: "<<((Flipflop*)&it )->getLastKapazitaet());
-
-                            }
-                            else if(buffer.find("tpdt:")==0)
-                            {
-                                ((Flipflop*)&it )->setGrundLaufzeit(atof(buffer.substr(5).c_str()));
-                                DEBUG_METHOD("ff tpdt init: "<<((Flipflop*)&it )->getGrundLaufzeit());
-
-                            }
-                            else if(buffer.find("kl:")==0)
-                            {
-                                ((Flipflop*)&it )->setLastFaktor(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD("ff kl init: "<<((Flipflop*)&it )->getLastFaktor());
-
-                            }
-                            else if(buffer.find("ct:")==0)
-                            {
-                                ((Flipflop*)&it )->setLastKapazitaetClock(atoi(buffer.substr(3).c_str()));
-                                DEBUG_METHOD("ff ct init: "<<((Flipflop*)&it )->getLastKapazitaetClock());
-
-                            }
-
-                            else
-                            {
-                                readError();
-                            }
-
+                        else if(buffer.find("tpd0:")==0)
+                        {
+                            it->setGrundLaufzeit(atof(buffer.substr(5).c_str()));
+                            DEBUG_METHOD("tpd0 init "<<it->getGrundLaufzeit());
                         }
 
 
+                        ///*Flipflop Attribute
+                        else if(buffer.find("tsetup:")==0)
+                        {
+                            ((Flipflop*)&it )->setSetupTime(atoi(buffer.substr(7).c_str()));
+                            DEBUG_METHOD("ff testup init: "<<((Flipflop*)&it )->getSetupTime());
 
+                        }
+                        else if(buffer.find("ed:")==0)
+                        {
+                            ((Flipflop*)&it )->setEingaenge(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD("ff ed init: "<<((Flipflop*)&it )->getEingaenge());
 
+                        }
+                        else if(buffer.find("thold:")==0)
+                        {
+                            ((Flipflop*)&it )->setHoldTime(atoi(buffer.substr(6).c_str()));
+                            DEBUG_METHOD("ff thold init: "<<((Flipflop*)&it )->getHoldTime());
 
+                        }
+                        else if(buffer.find("cd:")==0)
+                        {
+                            ((Flipflop*)&it )->setLastKapazitaet(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD("ff cd init: "<<((Flipflop*)&it )->getLastKapazitaet());
+
+                        }
+                        else if(buffer.find("tpdt:")==0)
+                        {
+                            ((Flipflop*)&it )->setGrundLaufzeit(atof(buffer.substr(5).c_str()));
+                            DEBUG_METHOD("ff tpdt init: "<<((Flipflop*)&it )->getGrundLaufzeit());
+
+                        }
+                        else if(buffer.find("kl:")==0)
+                        {
+                            ((Flipflop*)&it )->setLastFaktor(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD("ff kl init: "<<((Flipflop*)&it )->getLastFaktor());
+
+                        }
+                        else if(buffer.find("ct:")==0)
+                        {
+                            ((Flipflop*)&it )->setLastKapazitaetClock(atoi(buffer.substr(3).c_str()));
+                            DEBUG_METHOD("ff ct init: "<<((Flipflop*)&it )->getLastKapazitaetClock());
+
+                        }
+
+                        else
+                        {
+                            //Falls Attribut nicht gefunden
+                            readError();
+                        }
 
                     }
-                    break;
+
                 }
 
 
@@ -243,6 +240,7 @@ void Bibliothek::dateiAuswerten(void)
     }
 
 
+    //Ausgabe aller angelegten Elemente
     for (vector<GatterTyp>::iterator it = bibElemente.begin(); it!=bibElemente.end(); ++it)
     {
         DEBUG_METHOD("alle gt: "<<it->getName());
@@ -258,6 +256,7 @@ bool Bibliothek::pfadEinlesen(string pfad)
 {
 
     ifstream f(pfad.c_str());
+
     if(f.good())
     {
         datei = pfad;
@@ -269,19 +268,19 @@ bool Bibliothek::pfadEinlesen(string pfad)
         return false;
     }
 
-
 }
 
 /**Ausgabe einer Fehlermeldung beim Öﬀnen einer Datei. */
 void Bibliothek::openError(void)
 {
-    cerr <<"open error"<<endl;
+    cerr <<"OPEN ERROR"<<endl;
 
 }
 /**Ausgabe einer Fehlermeldung beim Lesen einer Datei.
 */
 void Bibliothek::readError(void)
 {
+    cerr <<"READ ERROR"<<endl;
 
 }
 
--- GatterTyp.cpp
+++ GatterTyp.cpp
@@ -16,43 +16,53 @@ void GatterTyp::setName(string n)
     name=n;
 
 }
+
 string GatterTyp::getName(void)
 {
     return name;
 }
+
 double GatterTyp::getGrundLaufzeit(void)
 {
     return grundLaufzeit;
 }
+
 short GatterTyp::getLastFaktor(void)
 {
     return lastFaktor;
 }
+
 short GatterTyp::getLastKapazitaet(void)
 {
     return lastKapazitaet;
 }
+
 short GatterTyp::getEingaenge(void)
 {
     return eingaenge;
 }
+
 bool GatterTyp::getIsFlipflop(void)
 {
     return false;
 }
 
+
 void GatterTyp::setGrundLaufzeit(double gl)
 {
     grundLaufzeit =gl;
 }
+
 void GatterTyp::setLastFaktor(short lf)
 {
     lastFaktor= lf;
 }
+
 void GatterTyp::setLastKapazitaet(short lk)
 {
     lastKapazitaet=lk;
 }
+
 void GatterTyp::setEingaenge(short ei)
 {
     eingaenge=ei;
